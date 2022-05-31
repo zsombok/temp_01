5548c73740ef3b83ee07afe07e9647f3c90866e0
zsombok <zsombokd@gamil.com>
2022-06-01 00:47:29 +0200

    Add basic layout

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-  win.loadFile('index.html')
-}
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+      nativeWindowOpen: true,
+    },
+    show: true,
+    // frame: false,
+    kiosk: true,
+    // resizable: false
+  });
+  win.fullScreen = false;
+  win.loadFile("index.html");
+
+  win.webContents.openDevTools({ mode: "right", activate: false });
+};
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows.length == 0) createWindow()
-  })
-})
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows.length == 0) createWindow();
+  });
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
